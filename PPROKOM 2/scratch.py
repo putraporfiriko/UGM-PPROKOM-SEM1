3bd3809afbfac0f227cfe99e7f27ed454f46e5a2
Abdul Hakeem <abdulhakeemputraporfiriko@mail.ugm.ac.id>
2023-08-26 20:35:48 +0700

    Update README.MD, add documentation images, scratch.py?

---
+++ PPROKOM 2/scratch.py
@@ -0,0 +1,6 @@
+var_x = input("Input a value: ")                # accepts input from the user.
+
+var_y = "This is the content of Variable Y."    # the value (or string, rather) is predetermined.
+
+
+
