719c6bfaae69db9601ea07edd698dd24306cc1e3
Abdul Hakeem <abdulhakeemputraporfiriko@mail.ugm.ac.id>
2023-08-28 08:28:14 +0700

    Add files

---
+++ PPROKOM 3/Code/latihan1.py
@@ -0,0 +1,5 @@
+feeling = input("How are you? ")
+if feeling.lower() == 'great':
+    print('I feel great too.') ## if there are no indentation, interpreter would spit out indentation error.
+else:
+    print('I hope the rest of your day is good.') ##this requires an indentation too.
