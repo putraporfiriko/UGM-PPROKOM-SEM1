5b9a601fafc7e649a35f07e30676eb77fb506120
Abdul Hakeem <abdulhakeemputraporfiriko@mail.ugm.ac.id>
2023-09-04 10:14:04 +0700

    Add: Pertemuan 4

---
+++ Pertemuan4_A2_Abdul Hakeem/Code/HackerRank/Tugas2.py
@@ -0,0 +1,11 @@
+x = int(input(""))
+y = int(input(""))
+z = int(input(""))
+
+avg = (x + y +z)/3
+
+if(avg>=50):
+    print("LULUS")
+
+else:
+    print("GAGAL")
---
+++ Pertemuan4_A2_Abdul Hakeem/Code/HackerRank/Tugas3.py
@@ -0,0 +1,9 @@
+x = int(input(""))
+y = int(input(""))
+z = int(input(""))
+if(x<y<z):
+    print(y)
+elif(x<z<y):
+    print(z)
+else:
+    print(x)
