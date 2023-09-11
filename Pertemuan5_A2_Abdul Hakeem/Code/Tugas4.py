810a115ee4595103bb3068269490e52e4b16a21f
Abdul Hakeem <abdulhakeemputraporfiriko@mail.ugm.ac.id>
2023-09-11 10:06:01 +0700

    add images and modify other files

--- Pertemuan5_A2_Abdul Hakeem/Code/Tugas4.py
+++ Pertemuan5_A2_Abdul Hakeem/Code/Tugas4.py
@@ -1,15 +1,11 @@
 datact = int(input("How much data you want to input? "))
 datalist = []
 
-
 for i in range (0, datact):
     datainput = int(input("Input the data: "))
     datalist.append(datainput)
-    print(datalist)
-# datalen = len(datalist) # cant put this early, because it'll register as 0 # don't need this, just use datact
 
 datasum = sum(datalist)
-
 
 dataavg = datasum / datact
 print(dataavg)    
